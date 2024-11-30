e6f937ba9efb53e2966d80047d41afabfd4ae6c5
ayush <flamedeveloper2021@gmail.com>
2024-11-30 13:12:54 +0530

    Input Saved to Database.

--- configs/schema.js
+++ configs/schema.js
@@ -1,8 +1,19 @@
-import { boolean, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { boolean, pgTable, serial, varchar, text } from "drizzle-orm/pg-core";
 
-export const USER_TABLE = pgTable('users',{
-    id: serial().primaryKey(),
-    name: varchar().notNull(),
-    email: varchar().notNull(),
-    isMember: boolean().default(false),
-})
+export const USER_TABLE = pgTable('users', {
+  id: serial().primaryKey(),
+  name: varchar().notNull(),
+  email: varchar().notNull(),
+  isMember: boolean().default(false),
+});
+
+export const STUDY_MATERIAL_TABLE = pgTable('studyMaterial', {
+  id: serial().primaryKey(),
+  courseId: varchar().notNull(),
+  courseType: varchar().notNull(),
+  topic: varchar().notNull(),
+  difficultyLevel: varchar().default('Easy'),
+  courseLayout: text(), // Use text to store JSON data as a string
+  createdBy: varchar().notNull(),
+  status: varchar().default('Generating'),
+});
